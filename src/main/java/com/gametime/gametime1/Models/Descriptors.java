c0a225c8726cf145d398eb82ed8008a96fd9d6d0
Ben Rybinski <45921913+benrybin@users.noreply.github.com>
2020-04-21 18:50:07 -0400

    adding games

--- src/main/java/com/gametime/gametime1/Models/Descriptors.java
+++ src/main/java/com/gametime/gametime1/Models/Descriptors.java
@@ -1,4 +1,7 @@
 package com.gametime.gametime1.Models;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Descriptors {
 }
--- src/main/java/com/gametime/gametime1/Models/Game.java
+++ src/main/java/com/gametime/gametime1/Models/Game.java
@@ -1,4 +1,17 @@
 package com.gametime.gametime1.Models;
 
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name="games")
 public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+    private String title;
+
+    private Set<String> descriptors;
+
+
 }
