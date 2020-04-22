f6858d7d6dd4a1bdd3d1703dd701abf111b26ef9
Ben Rybinski <45921913+benrybin@users.noreply.github.com>
2020-04-21 20:15:16 -0400

    tables created for games, tags and join table for them

--- src/main/java/com/gametime/gametime1/Models/Game.java
+++ src/main/java/com/gametime/gametime1/Models/Game.java
@@ -1,17 +1,24 @@
 package com.gametime.gametime1.Models;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Table(name="games")
+@Table(name="game")
 public class Game {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long Id;
     private String title;
+    @ManyToMany(cascade = CascadeType.ALL)
+            @JoinTable(
+                    name = "Game_Tags",
+                    joinColumns = {@JoinColumn(name="game_id")},
+                    inverseJoinColumns = {@JoinColumn(name="tags_id")}
+            )
+    Set<Tags> tags = new HashSet<>();
 
-    private Set<String> descriptors;
 
 
 }
--- src/main/java/com/gametime/gametime1/Models/Tags.java
+++ src/main/java/com/gametime/gametime1/Models/Tags.java
@@ -1,9 +1,8 @@
 package com.gametime.gametime1.Models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Tags {
@@ -12,5 +11,30 @@ public class Tags {
     private Long tagsId;
     private String tag;
     private Long gameid;
+    @ManyToMany(mappedBy = "tags")
+    private Set<Game> games = new HashSet<>();
 
+    public Long getTagsId() {
+        return tagsId;
+    }
+
+    public void setTagsId(Long tagsId) {
+        this.tagsId = tagsId;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public Long getGameid() {
+        return gameid;
+    }
+
+    public void setGameid(Long gameid) {
+        this.gameid = gameid;
+    }
 }
